c7a70b67d2257434141270f9e94a54ddf7456aef
Christopher <jokinha95@gmail.com>
2022-02-16 11:43:33 -0300

    Base do projeto

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Express } from "express"
+import cors from 'cors'
+import {AddressInfo} from 'net'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if(server){
+        const address = server.address() as AddressInfo
+        console.log(`server is running on port http://localhost:${address.port}`)
+    }
+    else{
+        console.log('failure upon starting server')
+    }
+})
+
+
+ 
